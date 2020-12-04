348882870e4dd92b6df715b39e952a51c71caf41
Christian Hernandez <chrissh443@gmail.com>
2020-12-04 16:15:20 -0400

    design improvements and added functions

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 /**
  * @name modApp
  * @version 0.0.1
- * @author @DevKhris */
+ * @author @DevKhris
+ */
 
-const sites = document.querySelectorAll("ul li a");
+//
+// Smooth scrolling functions
+// 
+const places = document.querySelectorAll("ul li a");
 
-for (let site of sites) {
+for (let place of places) {
     site.addEventListener("click", smoothScrolling);
 }
 
@@ -16,4 +20,28 @@ function smoothScrolling(e) {
     document
         .querySelector(href)
         .scrollIntoView({ behavior: "smooth", alignToTop: true });
+}
+
+//
+// Scroll to top functions
+// 
+var topButton = document.getElementById("scrollTopBtn");
+var rootElement = document.documentElement;
+
+topButton.addEventListener("click",topFunction);
+
+window.onscroll = function() {scrollFunction();};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
